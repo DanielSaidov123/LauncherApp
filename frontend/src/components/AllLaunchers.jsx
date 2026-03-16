a2785c7ffb1fc8df666458688a4c6370312fefe1
daniel <ddny3622@gmail.com>
2026-03-16 11:31:06 +0200

    add Pages '/'  HOME all launchers

--- frontend/src/components/AllLaunchers.jsx
+++ frontend/src/components/AllLaunchers.jsx
@@ -3,15 +3,39 @@ import { useLauncherStore } from "../store/launcherStore"
 
 export default function AllLaunchers() {
   
-    const {launchers ,getLaunchers } = useLauncherStore()
+    const {launchers ,getLaunchers ,loading ,error} = useLauncherStore()
 
     useEffect(()=>{
         getLaunchers()
     },[getLaunchers])
-    console.log(launchers)
     return (
-    <div>
-       
+    <div className="contaner">
+        {loading && <p>loading...</p>}
+        {error && <p>{error}</p>}
+       <table className="table">
+        <thead>
+            <tr className="row"> 
+                <th>id</th>
+                <th>name</th>
+                <th>rocketType</th>
+                <th>latitude</th>
+                <th>longitude</th>
+            </tr>
+        </thead>
+
+        <tbody>
+           {launchers.map((l)=>(
+             <tr key={l._id} className="row">
+                <td>{l._id}</td>
+                <td>{l.name}</td>
+                <td>{l.rocketType}</td>
+                <td>{l.latitude}</td>
+                <td>{l.longitude}</td>
+            </tr>
+           ))}
+        </tbody>
+
+       </table>
     </div>
   )
 }
