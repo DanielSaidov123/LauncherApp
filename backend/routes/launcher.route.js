538d1e48b7fbb0667631fbf96982f2def6e8ac8d
daniel <ddny3622@gmail.com>
2026-03-17 10:53:37 +0200

    add middleware

--- backend/routes/launcher.route.js
+++ backend/routes/launcher.route.js
@@ -1,13 +1,14 @@
 import express from "express"
 import { AllLaunchers, createLaunchers, DeleteLauncherByID, getLauncherByID, updateLauncher } from "../controllers/launcher.controller.js"
+import { checkAuth } from "../middleware/auth.middleware.js"
 
 const router = express.Router()
 
-router.get('/',AllLaunchers)
-router.post('/',createLaunchers)
-router.put('/:id',updateLauncher)
-router.get('/:id',getLauncherByID)
-router.delete('/:id',DeleteLauncherByID)
+router.get('/',checkAuth(["admin" ,"intel" ,"airforce"]),AllLaunchers)
+router.post('/' ,checkAuth(["admin" ,"intel"]),createLaunchers)
+router.put('/:id',checkAuth(["admin" ,"intel"]),updateLauncher)
+router.get('/:id',checkAuth(["admin" ,"intel" ,"airforce"]),getLauncherByID)
+router.delete('/:id',checkAuth(["admin" ,"intel"]),DeleteLauncherByID)
 
 
 export default router
